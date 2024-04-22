2fa34d6ed6ad213d02b28882774edb5ea10755db
matheusilva2023 <matheus.silva.1083821@sga.pucminas.br>
2024-04-21 22:47:54 -0300

    Adicionando tela de login em react

---
+++ src/react/GlobalStyles.js
@@ -0,0 +1,21 @@
+/* fonts */
+export const FontFamily = {
+  robotoRegular: "Roboto-Regular",
+};
+/* font sizes */
+export const FontSize = {
+  size_mini: 15,
+  size_mid: 17,
+};
+/* Colors */
+export const Color = {
+  colorWhite: "#fff",
+  colorGray: "#0f1419",
+  colorLightgray: "#cfd9de",
+  colorDimgray: "#536471",
+  colorDarkgray: "#959595",
+};
+/* border radiuses */
+export const Border = {
+  br_9980xl: 9999,
+};
