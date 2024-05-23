c60e4632a581964117fdec02fffbadc86ee3346e
matheusilva2023 <matheus.silva.1083821@sga.pucminas.br>
2024-05-23 00:47:10 -0300

    Ajustada api e publicada, ajustado consumo react dos dados para login.

--- src/backend/quacsBackEnd/quacsBackEnd/obj/Debug/net8.0/quacsBackEnd.AssemblyInfo.cs
+++ src/backend/quacsBackEnd/quacsBackEnd/obj/Debug/net8.0/quacsBackEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("quacsBackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04c6fc9be019640edf54c2ebea1d184f780e494b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d26bb2992239bf6a094991f8103d2aa4703869fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("quacsBackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("quacsBackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
